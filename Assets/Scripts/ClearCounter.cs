38976d07ac70930dae820f42f1ea9155d47733d5
igor100011 <104648867+igor10001@users.noreply.github.com>
2023-12-10 18:51:16 +0200

    add some Prefab Variants

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -9,8 +9,37 @@ public class ClearCounter : BaseCounter
 
    public override void Interact(Player player)
     {
-            
+        if (!HasKitchenObject())
+        {
+            // no kichen object on the counter
+            if(player.HasKitchenObject())
+            {
+                //player have carrying object 
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+
+            }
+            else
+            {
+                //player not carrying anything
+            }
+
+        }
+        else
+        {
+            // have kitchen object 
+            if(player.HasKitchenObject())
+            {
+                //Player is carrying something 
+            }
+            else
+            {
+                //Player is not carrying anything 
+                GetKitchenObject().SetKitchenObjectParent(player);    
+            }
+        }
+
+
     }
 
-    
+
 }
--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -11,10 +11,14 @@ public class ContainerCounter : BaseCounter  {
     
     public override void Interact(Player player)
     {
-        
+        if(!player.HasKitchenObject())
+        {
+            //Player is not carrying anything   
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
             kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
               OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
+        
 
          
     }
