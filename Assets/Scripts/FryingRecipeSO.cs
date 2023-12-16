4caf009f1813b969f65d9ddf459164fce2f871d6
igor10001 <104648867+igor10001@users.noreply.github.com>
2023-12-16 17:28:19 +0200

    change logic for egg recipe

--- Assets/Scripts/FryingRecipeSO.cs
+++ Assets/Scripts/FryingRecipeSO.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class FryingRecipeSO : ScriptableObject
 {
     public KitchenObjectSO input;
+    public KitchenObjectSO middle;
     public KitchenObjectSO output;
     public float fryingTimerMax;
         
--- Assets/Scripts/StoveCounter.cs
+++ Assets/Scripts/StoveCounter.cs
@@ -118,6 +118,9 @@ public class StoveCounter : BaseCounter, IHasProgress
                     player.GetKitchenObject().SetKitchenObjectParent(this);
 
                     fryingRecipeSO = GetFryingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+                    //middle visual
+                    GetKitchenObject().DestroySelf();
+                    KitchenObject.SpawnKitchenObject(fryingRecipeSO.middle, this);
 
                     state = State.Frying;
                     fryingTimer = 0f;
@@ -150,28 +153,7 @@ public class StoveCounter : BaseCounter, IHasProgress
             if (player.HasKitchenObject())
             {
                 //Player is carrying something 
-                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
-                {
-                    //Player is holding a plate
-
-                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
-                    {
-                        GetKitchenObject().DestroySelf();
-                        state = State.Idle;
-
-                        OnStateChanged?.Invoke(this, new OnStateChangedEventArgs
-                        {
-                            state = state
-                        });
-                        OnProgressChanged?.Invoke(this, new IHasProgress.OnProgressChangedEventArgs
-                        {
-                            progressNormalized = 0f
-                        });
-
-
-                    }
-
-                }
+               
 
             }
             else
