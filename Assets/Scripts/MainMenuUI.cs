ae0c46fa84b6c726b2b9ca5fd72dd5786ff22850
igor100011 <104648867+igor10001@users.noreply.github.com>
2023-12-19 13:11:28 +0200

    Add Loading

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -12,7 +12,7 @@ public class MainMenuUI : MonoBehaviour
     {
         playButton.onClick.AddListener(() =>
         {
-            SceneManager.LoadScene(1);
+            Loader.Load(Loader.Scene.GameScene);
         });
 
         quitButton.onClick.AddListener(() =>
